9ac1d23ebe74bb468501e674e13229e7880aa114
wojiushixiaobai <296015668@qq.com>
2019-06-25 18:47:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ rm -rf /config/tomcat9/logs/*
 cd /config/tomcat9/bin && ./startup.sh
 
 echo "Guacamole version 1.5.0, more see https://www.jumpserver.org"
-Quit the server with CONTROL-C.
+echo "Quit the server with CONTROL-C."
+sleep 10s
 tail -f /config/guacamole/data/log/info.log
